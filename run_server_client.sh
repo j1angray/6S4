4c8cee1683c4c8c1bfcfac4de99d191e89e9233d
Ruiyin Jiang <55040026+j1angray@users.noreply.github.com>
2020-08-09 05:06:29 -0400

    Update and rename run_sample.sh to run_server_client.sh

--- run_server_client.sh
+++ run_server_client.sh
@@ -1,4 +1,8 @@
 osascript -e 'tell app "Terminal"
+    do script "python3 Client.py 111"
+end tell'
+
+osascript -e 'tell app "Terminal"
     do script "cpython3 Node.py 1"
 end tell'
 
@@ -10,6 +14,3 @@ osascript -e 'tell app "Terminal"
     do script "python3 Node.py 3"
 end tell'
 
-osascript -e 'tell app "Terminal"
-    do script "python3 Client.py 111"
-end tell'
