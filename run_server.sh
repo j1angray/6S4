b130c195649da9c09002d076552e81beeb99f840
Ruiyin Jiang <55040026+j1angray@users.noreply.github.com>
2020-08-09 05:08:37 -0400

    Add files via upload
    change node configs to 5 and test

---
+++ run_server.sh
@@ -0,0 +1,19 @@
+osascript -e 'tell app "Terminal"
+    do script "python3 Node.py 1"
+end tell'
+
+osascript -e 'tell app "Terminal"
+    do script "python3 Node.py 2"
+end tell'
+
+osascript -e 'tell app "Terminal"
+    do script "python3 Node.py 3"
+end tell'
+
+osascript -e 'tell app "Terminal"
+    do script "python3 Node.py 4"
+end tell'
+
+osascript -e 'tell app "Terminal"
+    do script "python3 Node.py 5"
+end tell'
