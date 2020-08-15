f1852d82784665877feea1e248f29e3fbf40b565
Ruiyin Jiang <55040026+j1angray@users.noreply.github.com>
2020-08-15 03:03:19 -0400

    Shell file for only generating the server nodes

---
+++ runServer.sh
@@ -0,0 +1,20 @@
+osascript -e 'tell app "Terminal"
+    do script "cd /Folder address/6S4 ; python3 Node.py 1"
+end tell'
+
+
+osascript -e 'tell app "Terminal"
+    do script "cd /Folder address/6S4 ; python3 Node.py 2"
+end tell'
+
+osascript -e 'tell app "Terminal"
+    do script "cd /Folder address/6S4 ; python3 Node.py 3"
+end tell'
+
+osascript -e 'tell app "Terminal"
+    do script "cd /Folder address/6S4 ; python3 Node.py 4"
+end tell'
+
+osascript -e 'tell app "Terminal"
+    do script "cd /Folder address/6S4 ; python3 Node.py 5"
+end tell'
